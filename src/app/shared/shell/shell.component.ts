be695f66676818abf3a3782facb9713492f7eed5
Jonathan Gamble <jdgamble555@gmail.com>
2022-10-14 21:07:59 -0500

    logged out fix

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Auth, User, user } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-shell',
@@ -10,12 +11,18 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class ShellComponent {
 
+  user: Observable<User | null>;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
-
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private afa: Auth
+  ) {
+    this.user = user(this.afa);
+  }
 }
